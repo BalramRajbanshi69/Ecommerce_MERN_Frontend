bc8f5a402d9b65414bff2b4033be21b050d289b8
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-07-27 17:43:27 +0545

    hello

--- src/Components/AllProducts.jsx
+++ src/Components/AllProducts.jsx
@@ -7,12 +7,13 @@ import { useNavigate } from "react-router-dom";
 const AllProducts = () => {
   const apiUrl = import.meta.env.VITE_APP_API_URL;
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { data: products, searchTerm } = useSelector((state) => state.product);
 
-  const filteredSearchTerm = products.filter((product) =>
-    product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    product.description.toLowerCase().includes(searchTerm.toLowerCase())
+  const filteredSearchTerm = products.filter(
+    (product) =>
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      product.description.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   useEffect(() => {
@@ -25,10 +26,11 @@ const AllProducts = () => {
         <div className="container mx-auto max-w-7xl">
           <div className="flex flex-col py-8">
             <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {filteredSearchTerm && filteredSearchTerm.length > 0 &&
+              {filteredSearchTerm &&
+                filteredSearchTerm.length > 0 &&
                 filteredSearchTerm.map((product) => (
                   <div
-                  onClick={()=>navigate(`/singleProduct/${product._id}`)}
+                    onClick={() => navigate(`/singleProduct/${product._id}`)}
                     key={product._id}
                     className="overflow-hidden bg-white shadow-[0_8px_30px_rgb(0,0,0,0.12)]
                                transform transition-all duration-300 ease-in-out hover:-translate-y-2 hover:shadow-[0_20px_35px_rgb(0,0,0,0.2)] cursor-pointer"
@@ -37,11 +39,8 @@ const AllProducts = () => {
                     <div className="relative h-48">
                       <img
                         className="object-cover w-full h-full"
-                        src={
-                          product.productImage
-      ? product.productImage 
-      : s1
-                        }
+                        src={product.productImage ? product.productImage : s1}
+                        // src={product.productImage ? `${apiUrl}${product.productImage}` : s1}
                         alt={product.name || "Product Image"}
                       />
                     </div>
@@ -49,7 +48,9 @@ const AllProducts = () => {
                     {/* Content Container */}
                     <div className="p-4 text-black bg-white">
                       <div className="flex products-start justify-between">
-                        <h5 className="text-lg font-semibold">{product.name}</h5>
+                        <h5 className="text-lg font-semibold">
+                          {product.name}
+                        </h5>
                       </div>
 
                       <p className="mt-2 text-sm text-gray-600 line-clamp-2">
@@ -66,7 +67,6 @@ const AllProducts = () => {
                           {product.inStock}
                         </p>
                       </div>
-
                     </div>
                   </div>
                 ))}
@@ -79,11 +79,3 @@ const AllProducts = () => {
 };
 
 export default AllProducts;
-
-
-
-
-
-
-
-
